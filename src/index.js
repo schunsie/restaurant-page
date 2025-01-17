b65dfbc62804cec6fae0385a045a77ecd316ce84
schunsie <185822585+schunsie@users.noreply.github.com>
2025-01-17 13:08:18 +0100

    Add functionality to navigation

--- src/index.js
+++ src/index.js
@@ -2,16 +2,49 @@ import "./style.css"
 import { renderHome } from "./home"
 import { renderMenu } from "./menu"
 
-// Initial render
-// renderHome()
+// Initialize variables
+const content = document.querySelector('.content');
+let activePage = 'home';
 
-// test render
-renderMenu();
+// Initial render
+renderHome()
 
 // Navigation 
 const nav = document.querySelector('nav')
 nav.addEventListener('click', navHandling);
 
 function navHandling(event) {
-    console.log(event.target.id)
+    const t = event.target;
+    const targetID = t.id;
+
+    switch (targetID) {
+        case 'home':
+            toggleActive(t);
+            clearContent();
+            renderHome();
+            break;
+        case 'menu':
+            toggleActive(t);
+            clearContent();
+            renderMenu();
+            break;
+        // case 'Contact':
+        //     toggleActive(target);
+        //     clearContent();
+        //     renderContact();
+        //     break;
+    }
+}
+
+function clearContent() {
+    content.innerHTML = '';
+}
+
+function toggleActive(newActive) {
+    const oldActive = nav.querySelector(`#${activePage}`);
+    oldActive.classList.toggle('active')
+
+    // Set new active
+    newActive.classList.toggle('active');
+    activePage = newActive.id;
 }
